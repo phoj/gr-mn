cd6f4b4a28625b723ab364e9cb8c0195ba2a93d7
phoj <photos.900913@gmail.com>
2025-04-18 06:30:05 +0800

    0418

--- gr-mn.js
+++ gr-mn.js
@@ -1,2 +1,14 @@
+var arr
+get()
+function get(){
+ fetch('https://raw.githubusercontent.com/wbjon/2510/main/0-18.txt')
+ .then(res=>{console.log(res.status);return res.text()})
+ .then(data=>{
+  arr=data.split('\n')
+  console.log('欲讀取圖磚共',arr.length)
+ }).catch(err=>console.log("請求0-18.txt錯誤",err))
+ //setTimeout(get,3000)
+}
 
-console.log('Hello')
+
+

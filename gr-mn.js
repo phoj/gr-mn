c635d6194b3d52dc68117b92a1a3af532f35b04e
phoj <photos.900913@gmail.com>
2025-04-25 20:38:53 +0800

    0425

--- gr-mn.js
+++ gr-mn.js
@@ -1,10 +1,26 @@
 var http=require("http")
 var https=require("https")
+var readline=require('readline')
 var port=8080
 var message="startTime:"+new Date().toLocaleString('zh-TW',{timeZone:'Asia/Taipei'})+"(online_port:"+port+")"
 var Obj16={}
-var test=[2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720,2072321720]
 
+
+
+https.get('https://raw.githubusercontent.com/phoj/gr-mn/main/16_x_y_[42].txt',res=>{
+ const rl=readline.createInterface({input:res,crlfDelay:Infinity})
+ let sum=0
+ rl.on('line',line=>{
+  if(sum<100)console.log(++sum,line)
+ })
+ rl.on('close',()=>console.log('讀取完成'))
+})
+
+
+
+
+
+/*
 fetch('https://raw.githubusercontent.com/phoj/gr-mn/main/16.txt')
 .then(res=>{if(res.status!=200)console.error('錯誤碼',res.status);return res.text()})
 .then(data=>{
@@ -21,7 +37,7 @@ fetch('https://raw.githubusercontent.com/phoj/gr-mn/main/16.txt')
   console.log(key,value)
  }
 
-}).catch(err=>console.log("請求16.txt錯誤",err))
+}).catch(err=>console.log("請求16.txt錯誤",err))*/
 
 var proxyHandle=function(req,res){
  res.writeHead(200,{"content-type":"text/plain;charset=utf-8"});res.end(message)
